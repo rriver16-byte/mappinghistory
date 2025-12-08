f7d5fa1be6c7d192510ac19e479a35fee891506b
mlala <m.p.davis333@gmail.com>
2025-12-07 22:02:08 -0500

    api.js

--- api.js
+++ api.js
@@ -4,7 +4,7 @@
 
 function fetchLandmark(){
 
-    fetch("https://data.cityofnewyork.us/api/v3/views/buis-pvji/query.json?query=SELECT%20the_geom%2C%20objectid%2C%20borough%2C%20block%2C%20lot%2C%20address%2C%20bbl%2C%20lpc_name%2C%20lpc_lpnumb%2C%20lpc_sitede%2C%20lpc_sitest%2C%20lpc_altern%2C%20desdate%2C%20landmarkty%2C%20url_report%20WHERE%20((%60desdate%60%20%3E%20%272025-01-01%27)%20AND%20%60desdate%60%20IS%20NOT%20NULL)&app_token=naUk2Xt1bg1bqMjUEP74skogG")
+    fetch("https://data.cityofnewyork.us/api/v3/views/buis-pvji/query.json?query=SELECT%20the_geom%2C%20objectid%2C%20borough%2C%20block%2C%20lot%2C%20address%2C%20bbl%2C%20lpc_name%2C%20lpc_lpnumb%2C%20lpc_sitede%2C%20lpc_sitest%2C%20lpc_altern%2C%20desdate%2C%20landmarkty%2C%20url_report%20WHERE%20(`lpc_lpnumb`%20%3D%20%27LP-02653%27)&app_token=naUk2Xt1bg1bqMjUEP74skogG")
     .then((response) => response.json())
     .then((data) => {
         
@@ -42,4 +42,3 @@ function displayLandmark(data) {
 
 document.querySelector('#generate').addEventListener("click", fetchLandmark);
 
-fetchLandmark();
